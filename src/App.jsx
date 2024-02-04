66d96c3ab7c64de2c9e95ae08195ac0763276a3d
bob134552 <bob134552@hotmail.com>
2024-02-04 16:50:11 +0000

    Split components from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import reactImg from './assets/react-core-concepts.png';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)]
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>
+            Core Concepts
+          </h2>
+          <ul>
+          {
+            CORE_CONCEPTS.map((concept, index) => (
+              <CoreConcept
+                key={index}
+                {...concept}
+              />
+            ))
+          }
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
