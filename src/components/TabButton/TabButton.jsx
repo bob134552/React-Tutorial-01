5a56239f2912a15e53d842496e121324a28c147d
bob134552 <bob134552@hotmail.com>
2024-03-11 16:12:47 +0000

    Splitting app up into components

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,13 +1,12 @@
 import "./TabButton.css";
 
-export default function TabButton({children, onSelect, isSelected}) {
-    function clickHandler() {
-        console.log('TABBUTTON COMPONENT EXECUTING');
-    }
-
+export default function TabButton({ children, isSelected, ...props }) {
+    console.log('TABBUTTON COMPONENT EXECUTING');
     return (
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} {...props}>
+                {children}
+            </button>
         </li>
     );
-};
+}
